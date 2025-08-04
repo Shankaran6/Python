8ca7d173283a89e7e358fdf5c49c68301acdca57
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-04 16:12:05 +0530

    did some stuff

---
+++ Python codes/HW.py
@@ -0,0 +1,43 @@
+#phone reload system:
+#For first 30 min 2/= per minute 
+#for 31 - 60 min 1.50/=
+# 60 to 120 - 1/=
+# above 120  0.5/=
+
+# time = int(input("please enter the minutes talked"))
+
+# while time<0:
+#     time=int(input("Please enter a valid time"))
+# if time<=30:
+#     cost= time*2
+# elif 30<time <=60:
+#     cost= 60 + 1.5* (time-30)
+# elif 60<time <=120:
+#     cost= 105 + 1* (time-60)
+# elif 120<time:
+#     cost= 165 + 0.5* (time-120)
+    
+# print(cost)
+
+#________________________________________________________________
+
+#1-90 7/=
+#91-150  10/=
+# 151-300. 15/=
+#300 + 15/= + 3%
+
+units = int(input("Please enter the number of units used"))
+
+while units<0:
+    units = int(input("Please enter a valid number"))
+
+if units<91:
+    cost=units*7
+elif units<151:
+    cost=630 + (units-90)*10
+elif units<301:
+    cost=1230 + (units-150)*15
+elif units>300:
+    cost=(3480 +(units-300)*15)*103/100
+
+print(cost, "/=")
--- Python codes/if_condition.py
+++ Python codes/if_condition.py
@@ -1,12 +1,59 @@
-print("line 1")
-if True:
-    print("line 2") # 4 spaces or a tab 
-print("line 3")
+# print("line 1")
+# if True:
+#     print("line 2") # 4 spaces or a tab 
+# print("line 3")
 
-print("line 1")
-if False:
-    print("line 2") # 4 spaces or a tab 
-print("line 3")
+# print("line 1")
+# if False:
+#     print("line 2") # 4 spaces or a tab 
+# print("line 3")
 
-if True:
-    print("Need at least one line inside if condition")
+# if True:
+#     print("Need at least one line inside if condition")
+
+
+# x = input("please enter True or False")
+# if x=="False":
+#     print("Hi")
+# elif x=="True":
+#     print("Hello")
+
+# is_bool= bool(input("Enter True or False"))
+# print(is_bool)
+# if is_bool:
+#     print("Hi")
+# else:
+#     print("Hello")
+
+
+
+# if marks_< 45:
+#     print("fail")
+# elif marks_ <55:
+#     print("S")
+# elif marks_ <65:
+#     print("C")
+# elif marks_ <75:
+#     print("B")
+# else:
+#     print("A")
+marks_=int(input("please enter your marks:"))
+if marks_<0 or marks_>100:
+    print("Invalid marks")
+else:
+    if 0<=marks_<45:
+        print("Fail")
+    else:
+        if marks_<55:
+            print("S")
+        else:
+            if marks_<65:
+                print("C")
+            else:
+                if marks_<75:
+                    print("B")
+                else:
+                    if 75<= marks_ <=100:
+                        print("A")
+
+
