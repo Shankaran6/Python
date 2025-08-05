ba287a63d97ebce71d7623664eca3c2740127952
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-05 16:29:32 +0530

    User login, registration system

--- Python codes/hi.py
+++ Python codes/hi.py
@@ -1,8 +1,37 @@
-x= int(input("Enter the first number:"))
-y=int(input("Enter the second number:"))
+print("Customer registration system")
+n = int(input("Please enter the total number of members in team"))
+names =[]
+passwords=[]
+c=0
+attempt=0
+while c < n:
+    func= str(input("Please enter R for registering and L for login"))
+    if func == "R":
+        name = str(input("Please enter the name of member"))
+        names.append(name)
+        c+=1
+        print(len(names),"is your number")
+        password= str(input("Please enter password"))
+        passwords.append(password)
+    elif func =="L":
+        num=int(input("please enter your number"))
+        print(f"Good morning {names[num-1]}.")
+        pas = str(input("please enter your password:"))
+        while attempt<5:
+            if pas == passwords[num-1]:
+                print("logged in")
+                attempt=0
+            else:
+                print("Incorrect password")
+                attempt+=1
 
-if x>y:
-    print("The first number is greater")
-else:
-    print("Second number is greater")
+
+
+
+
+
+
+
+
+    
     
