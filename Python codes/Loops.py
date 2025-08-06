69d6de28676278936c81ef9194416c34ac321596
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-06 15:31:07 +0530

    update

--- Python codes/Loops.py
+++ Python codes/Loops.py
@@ -1,12 +1,28 @@
 #for loop
 #while loop
+# x=1
+# isbool = True
+# while isbool:
+#     print(x)
+#     x+=1
+#     if x==5:
+#         isbool = False
+
+# x=0
+
+# while x < 10:
+#     print("Shankaran")
+#     x+=1
 x=1
-isbool = True
-while isbool:
-    print(x)
+while x < 5:
+    y=1
+    while y <6:
+        print(y,end='')
+        y+=1
+    print("\n")
     x+=1
-    if x>5:
-        isbool = False
+
+
     
 
 
