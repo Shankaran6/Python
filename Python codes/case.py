b6d0037f0845920d5100648638597afd3bf02d8b
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-14 16:03:13 +0530

    casefile

---
+++ Python codes/case.py
@@ -0,0 +1,35 @@
+# S="Welcome To Python"
+
+# print("1.",S)
+# print("2.",S.lower())
+# print("3.",S.upper())
+# print("4.",S.title())
+# print("5.",S.capitalize())
+# print("6.",S.swapcase())
+# print("7. is alpha-",S.isalpha())
+# print("8. is digit-",S.isdigit())
+# print("9. is lower-",S.islower())
+# print("10.is upper-",S.isupper())
+# print("11.is space-",S.isspace())
+# print("12.startwith-",S.startswith("Welcome"))
+# print("13.endwith-",S.endswith('n'))
+# print("14.find-",S.find('To'))
+# print("15.index-",S.index('To'))
+# print("16.count-",S.count('t'))
+# print("17.",S.replace('Python','Java'))
+# print("18.",S.strip())
+# print("19.",S.lstrip())
+# print("20.",S.rstrip())
+# print("21.",S.split())
+
+
+# num = input("Please enter a number")
+
+# while not (num.isdigit()):
+#     print("invalid number")
+#     num=input("Please enter a valid number")
+
+# print(num)
+
+
+
