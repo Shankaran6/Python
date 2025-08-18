51d78289482514c20a4d6b2d91c140671eac1a7a
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-18 15:55:03 +0530

    list

---
+++ Python codes/list.py
@@ -0,0 +1,13 @@
+Subjects=["Math","Tamil","English","Science","IT"]
+
+
+# for i in range(0,5):
+#     print(Subjects[i],end=', ')
+
+# print(Subjects[-5])
+
+# print(len(Subjects))
+n=0
+while n<5:
+    print(Subjects[n],end=', ')
+    n+=1
