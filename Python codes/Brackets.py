b85a33a04928eb1654fd16a888b859e81b0aaada
Shankaran6 <guruparanshankaran@gmail.com>
2025-08-27 16:23:35 +0530

    sets

--- Python codes/Brackets.py
+++ Python codes/Brackets.py
@@ -1,22 +1,63 @@
-# S={"Maths", "Tamil", "English"}
-# print(S)
-# print(type(S))
+# # # S={"Maths", "Tamil", "English"}
+# # # print(S)
+# # # print(type(S))
 
-S={"Maths", "Tamil", "English","Maths"} #unordered and not indexed
+# # S={"Maths", "Tamil", "English","Maths"} #unordered and not indexed
 
-print(S)
-print(type(S))
+# # print(S)
+# # print(type(S))
 
-S.add("IT") #for single values
-print(S)
+# # S.add("IT") #for single values
+# # print(S)
 
-S.update(["History","Sinhala","Literature"]) #multiple values
-print(S)
+# # S.update(["History","Sinhala","Literature"]) #multiple values
+# # print(S)
 
-S.remove("Math")
-print(S)
+# # S.remove("Maths")
+# # print(S)
+
+# # S.discard("Math")
+# # print(S)
+
+# # S.pop()
+# # print(S)
+
+# # S.clear()
+# # print(S)
 
 
+# #add, update, remove, discard
 
+# # numbers=[70, 90, 26, 70, 55, 90, 76, 55, 38]
+# # s=set(numbers)
+# # print(s)
 
-#add, update, remove
+# a={1,2,3,4,5}
+# b={3,5,7,8,9}
+
+# # c=a|b 
+# # c=a.union(b)
+# # Q=a&b
+# # Q=a.intersection(b)
+# # print(Q)
+
+# # e=a-b    #AnB'
+# # e=b-a     
+# # print(e)
+
+# f=a^b      #(AUB)n(AnB)'
+# print(f)
+
+a={1,2,3}
+b={1,2,3,4,5}
+c={4,5,6}
+
+d=a|b|c
+e=a&b&c
+print(d)
+print(e)
+print(a<=b) #a is a sub set of b
+print(b>=a)
+print(a.issubset(b))
+print(b<=a)
+print(a.issuperset(b))
