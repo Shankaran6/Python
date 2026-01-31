eae33d3e68e26958dc5fe7d5a04f23b484af7549
Shankaran6 <guruparanshankaran@gmail.com>
2026-01-31 15:39:23 +0530

    XOX game
    work in progress

---
+++ Python codes/XOX.py
@@ -0,0 +1,58 @@
+def selector():
+    if ask<5:
+        enter= ask+4
+    else:
+        enter= ask-4
+    if enter in table[1]:
+        table[1][table[1].index(enter)]="O"
+    elif enter in table[2]:
+        table[2][table[2].index(enter)]="O"
+
+
+import random
+table=[[0],[1,3,5,7],[2,4,6,8]]
+number1=[0,1,2,3]
+number2=[0,1,2,3]
+x=0
+
+ask=int(input("Please enter the position of X:"))
+if ask==0:
+    table[0][0]="X"
+    enter=random.choice(number1)
+    number1.remove(enter)
+    table[1][enter]="O"
+print(table)
+
+ask=int(input("Please enter the position of X:"))
+if ask in table[1]:  
+    enter=table[1].index(ask)  
+    table[1][enter]="X"
+    number1.remove(enter)
+    selector()
+
+
+ask=int(input("Please enter the position of X:"))
+if ask in table[1]:  
+    enter=table[1].index(ask)  
+    table[1][enter]="X"
+    number1.remove(enter)
+for k in range (0,3):
+    if table[1][k]=="X" and table[1][k+1]=="X":
+        table[2][k+1]=="O"
+
+
+print(table)
+    
+    
+
+
+
+
+
+
+
+
+
+
+        
+
